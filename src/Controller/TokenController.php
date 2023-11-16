c2e52cd5645a3866cd52c42a4b5cfb798a1e5327
Simeon Goranov <sgoranov@users.noreply.github.com>
2023-11-16 17:14:58 +0200

    Add TokenController

--- src/Controller/TokenController.php
+++ src/Controller/TokenController.php
@@ -4,22 +4,54 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use League\OAuth2\Server\AuthorizationServer;
+use League\OAuth2\Server\Exception\OAuthServerException;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
+use Symfony\Bridge\PsrHttpMessage\HttpFoundationFactoryInterface;
+use Symfony\Bridge\PsrHttpMessage\HttpMessageFactoryInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TokenController extends AbstractController
 {
-    #[Route('/token', name: 'oauth2_token')]
-    public function index(AuthorizationServer $server): Response
+    private AuthorizationServer $server;
+
+    private HttpFoundationFactory $httpFoundationFactory;
+
+    private ResponseFactoryInterface $responseFactory;
+
+    private HttpMessageFactoryInterface $httpMessageFactory;
+
+    public function __construct(
+        AuthorizationServer $server,
+        HttpFoundationFactoryInterface $httpFoundationFactory,
+        HttpMessageFactoryInterface $httpMessageFactory,
+        ResponseFactoryInterface $responseFactory
+    )
     {
+        $this->server = $server;
+        $this->httpFoundationFactory = $httpFoundationFactory;
+        $this->httpMessageFactory = $httpMessageFactory;
+        $this->responseFactory = $responseFactory;
+    }
 
-        var_dump($server);
-        exit();
+    #[Route('/token', name: 'oauth2_token')]
+    public function index(Request $request): Response
+    {
+        $psrRequest = $this->httpMessageFactory->createRequest($request);
+        $psrResponse = $this->responseFactory->createResponse();
 
+        try {
 
-        return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
-        ]);
+            $response = $this->server->respondToAccessTokenRequest($psrRequest, $psrResponse);
+
+        } catch (OAuthServerException $exception) {
+
+            $response = $exception->generateHttpResponse($psrResponse);
+        }
+
+        return $this->httpFoundationFactory->createResponse($response);
     }
 }
