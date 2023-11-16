dbcbd8c907afe81f171c54b550ba31265c856c3b
Simeon Goranov <sgoranov@users.noreply.github.com>
2023-11-16 17:14:58 +0200

    Setup Symfony skeleton app

---
+++ docker/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Modify /etc/hosts
+DEFAULT_ROUTE=$(ip route show default | awk '/default/ {print $3}')
+echo "$DEFAULT_ROUTE localhost.container.com" >> /etc/hosts
+
+cd /var/www/
+
+# Run composer install and replace the correct configuration
+# rm -rf vendor && \
+composer install --no-scripts && \
+
+# Database setup
+export PGHOST=database-server
+export PGUSER=admin
+export PGPASSWORD=admin
+until psql -c "\q"; do sleep 3; done
+echo "SELECT 'CREATE DATABASE idp' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'idp')\gexec" \
+ | psql -v ON_ERROR_STOP=1
+
+php bin/console -e dev doctrine:migrations:migrate
+
+# Set correct permissions on var/
+rm -rf var/cache/*
+chmod -R o+rw var/
+
+# This will exec the CMD from Dockerfile
+exec "$@"
